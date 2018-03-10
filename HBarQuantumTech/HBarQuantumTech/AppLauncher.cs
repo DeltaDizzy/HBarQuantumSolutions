c9289d5a044f418da4ecaaf64c5ba5941a5e5e2a
DeltaDizzy <EJBraswell@gmail.com>
2018-03-10 17:31:02 -0600

    yeeeee

---
+++ HBarQuantumTech/HBarQuantumTech/AppLauncher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KSP.UI.Screens;
+using UnityEngine;
+
+namespace HBarQuantumTech
+{
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    class AppLauncherStuff : MonoBehaviour
+    {
+        private ApplicationLauncherButton toolbarButton;
+        private Texture2D toolbarTex;
+
+        public void Awake()
+        {
+            Debug.Log("[HBAR]: Awaking Instance");
+            DontDestroyOnLoad(this);
+        }
+
+        public void AppLauncherGen()
+        {
+            if (ApplicationLauncher.Ready && toolbarButton == null)
+            {
+                //toolbarButton =
+                    //ApplicationLauncher.Instance.AddModApplication();
+            }
+        }
+    }
+}
---
+++ HBarQuantumTech/HBarQuantumTech/ModuleQuantumJiggle.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace HBarQuantumTech
+{
+    public class ModuleQuantumJiggle : PartModule
+    {
+        public static Vector3d ForceToApply;
+        public static float speed;
+
+        /*public static float SpeedGen()
+        {
+            float speed = Random.value;
+            return speed;
+        }*/
+        public static Vector3d ForceDirGen()
+        {
+            Vector3d ForceDir = Random.onUnitSphere;
+            return ForceDir;
+        }
+
+        public void JiggleGen()
+        {
+            /*foreach (Vessel currentVessel in FlightGlobals.VesselsLoaded)
+            {
+                //force = vessel mass: Force = vessel.GetTotalMass() * desiredAcceleration
+                ForceToApply *= (currentVessel.GetTotalMass() * 5);
+                this.part.AddForceAtPosition(ForceToApply, currentVessel.CoM);
+            }*/
+            int loadedVesselCount = FlightGlobals.VesselsLoaded.Count();
+            for (int index = 0; index < loadedVesselCount; index++)
+            {
+                Vessel currentVessel = FlightGlobals.VesselsLoaded[index];
+                ForceToApply *= (currentVessel.GetTotalMass() * 5 * TimeWarp.fixedDeltaTime);
+                this.part.AddForceAtPosition(ForceToApply, currentVessel.CoM);
+            }
+        }
+        public void FixedUpdate()
+        {
+            this.JiggleGen();
+        }
+    }
+}
